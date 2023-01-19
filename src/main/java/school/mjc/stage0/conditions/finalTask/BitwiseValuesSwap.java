af51ce4379873476630afc4db0c5853b7bb8a3f4
Assetjan <asetziyabek@gmail.com>
2023-01-19 22:57:09 +0600

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = first ^ second;
+        second = first ^ second;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
